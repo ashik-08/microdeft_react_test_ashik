255f66f9947963e9316868d0afaaab002c8ad568
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-27 09:40:06 +0600

    [feat](main): simplify routing by removing unnecessary Routes component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <App />
+    </Router>
   </StrictMode>
 );
