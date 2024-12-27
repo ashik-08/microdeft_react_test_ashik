cd875ff4c61d574c41657e3f06df88dd4c74cb46
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-27 09:39:45 +0600

    [feat](App): implement routing with MainLayout and ErrorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+// import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-green-400">Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        {/* <Route index element={<HomePage />} /> */}
+        {/* <Route path="add-course" element={<AddCourse />} /> */}
+        {/* <Route path="course-list" element={<CourseList />} /> */}
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
