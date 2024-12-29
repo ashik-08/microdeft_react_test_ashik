f6e89af9f7bc35491fbed420e84899573f3ff4ca
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-29 08:22:50 +0600

    [feat](tailwind): remove unused primary color definitions from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        // "primary-400": "#34B778",
-        // "primary-500": "#1BAE6C",
-        // "primary-600": "#0F8C56",
         "primary-600": "#257BEC",
         "primary-700": "#1C64D9",
         dark: "#252525",
